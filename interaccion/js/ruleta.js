ca6ca7bdcb08ccdfdd04901754060a809293abdb
Carolina <carolinac982[1;5r""[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C2@hotmail.com>
2019-05-24 03:50:29 -0500

    Creacion del formato de trabajo con Typecript

---
+++ interaccion/js/ruleta.js
@@ -0,0 +1,9 @@
+"use strict";
+function degrees(radianes) {
+    return radianes * 180 / Math.PI;
+}
+;
+function radianes(grados) {
+    return grados * Math.PI / 180;
+}
+;
---
+++ interaccion/typescript/ruleta.ts
@@ -0,0 +1,7 @@
+function degrees(radianes: number) {
+    return radianes * 180 / Math.PI;
+};
+
+function radianes(grados: number) {
+    return grados * Math.PI / 180;
+};
