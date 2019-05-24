4e70a96f426b478e9dd73a8abe7403beb036a151
Carolina <carolinac982[1;5r""[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C2@hotmail.com>
2019-05-24 03:51:11 -0500

    Creacion de a clase de la ruleta que se ocupa de la actividad principal

--- interaccion/js/ruleta.js
+++ interaccion/js/ruleta.js
@@ -7,3 +7,45 @@ function radianes(grados) {
     return grados * Math.PI / 180;
 }
 ;
+var Ruleta = /** @class */ (function () {
+    function Ruleta() {
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = 1280;
+        this.canvas.height = 720;
+        this.stage = new createjs.Stage(this.canvas);
+        this.stage.update();
+        this.ruleta = new createjs.Shape();
+        this.contenedor = new createjs.Container();
+        this.mascatas = new Array();
+        this.contenedor.addChild(this.ruleta);
+        this.stage.addChild(this.contenedor);
+        this.movimiento = new createjs.Tween(this.contenedor, { loop: -1 });
+        this.iniciar();
+    }
+    Ruleta.prototype.iniciar = function () {
+        this.contenedor.x = this.canvas.width / 2;
+        this.contenedor.y = this.canvas.height / 2;
+        this.ruleta.graphics
+            .beginFill("green")
+            .drawCircle(0, 0, 300)
+            .beginFill("red")
+            .drawCircle(0, 0, 100)
+            .beginFill("blue")
+            .drawCircle(200, 0, 10);
+        this.movimiento.to({ rotation: 360 }, 5000);
+        this.stage.on("stagemousedown", function () {
+            // this.movimiento.paused = !this.movimiento.paused;
+        });
+        createjs.Ticker.addEventListener("tick", this.stage);
+        this.stage.update();
+    };
+    Ruleta.prototype.agregar = function () {
+        var pet = new Mascotas(this);
+        this.stage.update();
+    };
+    Ruleta.prototype.incluirEn = function (ubicacion) {
+        var elemento = document.querySelector(ubicacion);
+        elemento.append(this.canvas);
+    };
+    return Ruleta;
+}());
--- interaccion/typescript/ruleta.ts
+++ interaccion/typescript/ruleta.ts
@@ -5,3 +5,75 @@ function degrees(radianes: number) {
 function radianes(grados: number) {
     return grados * Math.PI / 180;
 };
+
+class Ruleta {
+
+    stage: createjs.Stage;
+    canvas: HTMLCanvasElement;
+
+    contenedor: createjs.Container;
+    ruleta: createjs.Shape;
+    movimiento: createjs.Tween;
+    mascatas: Array<Mascotas>;
+
+    constructor() {
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = 1280;
+        this.canvas.height = 720;
+        this.stage = new createjs.Stage(this.canvas);
+        this.stage.update();
+
+        this.ruleta = new createjs.Shape();
+
+        this.contenedor = new createjs.Container();
+        this.mascatas = new Array();
+
+        this.contenedor.addChild(this.ruleta);
+        this.stage.addChild(this.contenedor);
+
+        this.movimiento = new createjs.Tween(this.contenedor, { loop: -1 });
+
+        this.iniciar();
+
+    }
+
+    iniciar() {
+        this.contenedor.x = this.canvas.width / 2;
+        this.contenedor.y = this.canvas.height / 2;
+        this.ruleta.graphics
+            .beginFill("green")
+            .drawCircle(0, 0, 300)
+            .beginFill("red")
+            .drawCircle(0, 0, 100)
+            .beginFill("blue")
+            .drawCircle(200, 0, 10);
+
+
+        this.movimiento.to({ rotation: 360 }, 5000);
+
+        this.stage.on("stagemousedown", () => {
+            // this.movimiento.paused = !this.movimiento.paused;
+        });
+
+        createjs.Ticker.addEventListener("tick", this.stage);
+        this.stage.update();
+    }
+
+    agregar() {
+        let pet = new Mascotas(this);
+       
+
+     
+
+
+        this.stage.update();
+
+
+    }
+
+    incluirEn(ubicacion: string) {
+        let elemento: HTMLElement = <HTMLElement>document.querySelector(ubicacion);
+        elemento.append(this.canvas);
+
+    }
+}
